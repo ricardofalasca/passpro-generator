7dd6c70401863c28463ae793d3540aaf5b28e442
Ricardo Falasca <ricardo@falasca.com.br>
2018-04-03 19:39:40 -0300

    fix: remove dumb test

--- passpro/test.py
+++ passpro/test.py
@@ -7,7 +7,7 @@ class PasswordGenerateTestCase(TestCase):
         self._min = 5
         self._max = 15
 
-    def test_as_instance(self):
+    def test_password_generate(self):
         _quantity = 1000
 
         # initialize an instance
@@ -18,9 +18,3 @@ class PasswordGenerateTestCase(TestCase):
         self.assertEqual(len(pwds), _quantity)
         [self.assertTrue(all([len(p) >= self._min, len(p) <= self._max]))
          for p in pwds]
-
-    def test_as_class_method(self):
-        pwd = PasswordPronounceable.new(self._min, self._max)
-
-        self.assertIsNotNone(pwd)
-        self.assertTrue(all([len(pwd) >= self._min, len(pwd) <= self._max]))
